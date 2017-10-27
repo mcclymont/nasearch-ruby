b4824bcee962a28275b442d065a36e54230699cb
Chris McClymont <chris@mcclymont.it>
2017-10-27 14:19:40 -0700

    Extract OPML parser logic to separate module

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,38 +1,32 @@
 class Source < ApplicationRecord
   belongs_to :show
   before_validation :set_show!
+  after_initialize :include_module
+
+  NEW_HTML_FORMAT_START = 590
+
+  def include_module
+    return if respond_to? :process_text!
+
+    if file_type == 'opml'
+      extend ::Loaders::OPML
+    elsif file_type == 'html'
+      if show_id <= NEW_HTML_FORMAT_START
+        extend ::Loaders::NewHTML
+      else
+        Rails.logger.warning "File type #{file_type} not implemented"
+      end
+    else
+      Rails.logger.warning "File type #{file_type} not implemented"
+    end
+  end
+
+  def file_type=(val)
+    super(val).tap { include_module }
+  end
 
   def inspect
     "#<Source show_id: #{show_id} file_type: #{file_type}>"
-  end
-
-  def strip_html(input)
-    text = CGI.unescapeHTML(input)
-    if text.start_with?('<')
-      text = Nokogiri::XML(text).children[0].text
-    end
-    text
-  end
-
-  def extract_html(input)
-    text = CGI.unescapeHTML(input)
-    Nokogiri::XML(text).children[0]
-  end
-
-  def extract_title
-    xml = Nokogiri::XML(text)
-    unless xml.errors.empty?
-      return puts "Nokogiri errors for episode #{show_id}"
-    end
-
-    title = xml.at_css('head title').content
-    if show_id == 726
-      'Weather Whiplash' # Quotes weren't closed
-    elsif show_id == 589
-      nil # Not available
-    else
-      CGI.unescapeHTML(title).match(/"(.*)"/)[1]
-    end
   end
 
   def set_show!
@@ -44,80 +38,5 @@ class Source < ApplicationRecord
     note_ids = show.notes.pluck(:id)
     UrlEntry.where(note: note_ids).delete_all
     Note.where(show: show).delete_all
-  end
-
-  def process_text!
-    ActiveRecord::Base.transaction do
-      set_show! unless show.present?
-      delete_notes!
-
-      xml = Nokogiri::XML(text)
-      start = xml.at_xpath("/opml/body/outline[@type='tabs']")
-
-      shownotes = start.xpath("outline[@text='Shownotes']/outline")
-      clips = start.xpath("outline[@text='CLIPS & DOCS']/outline")
-
-      (shownotes + clips).each do |topic_node|
-        next if topic_node['text'].start_with?('<')
-
-        topic = topic_node['text']
-        notes = topic_node.xpath('outline')
-
-        if notes.all? { |note| note.children.empty? && !note['text'].starts_with?('<') }
-          # We aren't going to get a separate title and then further
-          # children nodes with the actual text
-          # This is the text - and we didn't get a title node.
-          notes = [topic_node]
-        end
-
-        notes.each do |note_node|
-          note = show.notes.new(
-            topic: topic,
-            title: strip_html(note_node['text'])
-          )
-
-          children = note_node.children
-
-          children = [note_node] if children.empty?
-
-          urls = []
-          entries = children.map do |entry_node|
-            text = entry_node['text']
-            next if text.blank?
-            next if [
-              '").addClass(n).attr(',
-              '\n {{ more.more_rank',
-              '-1)r&&r.push(o);else'
-            ].any? { |str| text.starts_with? str }
-
-            if entry_node.key?('url')
-              url = entry_node['url']
-              if text.blank?
-                text = File.basename(URI.parse(url).path)
-              end
-              urls << {text: text, url: url}
-              text = "<a href='#{url}'>#{text}</a>"
-            end
-
-            next if text.blank?
-
-            text
-          end.compact
-
-          if entries.any? || urls.any?
-            note.text = entries.join("\n")
-
-            if note.title.blank? && urls.length == 1
-              url = urls.first[:url]
-              extension = File.extname(url)
-              note.title = "File: #{extension[1..-1]}" unless extension.blank?
-            end
-
-            note.save!
-            note.url_entries.create!(urls)
-          end
-        end
-      end
-    end
   end
 end
---
+++ app/src/loaders/opml.rb
@@ -0,0 +1,109 @@
+module Loaders::OPML
+  def strip_html(input)
+    text = CGI.unescapeHTML(input)
+    if text.start_with?('<')
+      text = Nokogiri::XML(text).children[0].text
+    end
+    text
+  end
+
+  def extract_html(input)
+    text = CGI.unescapeHTML(input)
+    Nokogiri::XML(text).children[0]
+  end
+
+  def extract_title
+    xml = Nokogiri::XML(text)
+    unless xml.errors.empty?
+      return puts "Nokogiri errors for episode #{show_id}"
+    end
+
+    title = xml.at_css('head title').content
+    if show_id == 726
+      'Weather Whiplash' # Quotes weren't closed
+    elsif show_id == 589
+      nil # Not available
+    else
+      CGI.unescapeHTML(title).match(/"(.*)"/)[1]
+    end
+  end
+
+  def process_text!
+    ActiveRecord::Base.transaction do
+      set_show! unless show.present?
+      delete_notes!
+
+      xml = Nokogiri::XML(text)
+      start = xml.at_xpath("/opml/body/outline[@type='tabs']")
+
+      shownotes = start.xpath("outline[@text='Shownotes']/outline")
+      clips = start.xpath("outline[@text='CLIPS & DOCS']/outline")
+
+      if shownotes.length + clips.length == 0
+        puts 'no shownotes!'
+      end
+
+      (shownotes + clips).each do |topic_node|
+        next if topic_node['text'].start_with?('<')
+
+        topic = topic_node['text']
+        notes = topic_node.xpath('outline')
+
+        if notes.all? { |note| note.children.empty? && !note['text'].starts_with?('<') }
+          # We aren't going to get a separate title and then further
+          # children nodes with the actual text
+          # This is the text - and we didn't get a title node.
+          notes = [topic_node]
+        end
+
+        notes.each do |note_node|
+          note = show.notes.new(
+            topic: topic,
+            title: strip_html(note_node['text'])
+          )
+
+          children = note_node.children
+
+          children = [note_node] if children.empty?
+
+          urls = []
+          entries = children.map do |entry_node|
+            text = entry_node['text']
+            next if text.blank?
+            next if [
+              '").addClass(n).attr(',
+              '\n {{ more.more_rank',
+              '-1)r&&r.push(o);else'
+            ].any? { |str| text.starts_with? str }
+
+            if entry_node.key?('url')
+              url = entry_node['url']
+              if text.blank?
+                text = File.basename(URI.parse(url).path)
+              end
+              urls << {text: text, url: url}
+              text = "<a href='#{url}'>#{text}</a>"
+            end
+
+            next if text.blank?
+
+            text
+          end.compact
+
+          if entries.any? || urls.any?
+            note.text = entries.join("\n")
+
+            if note.title.blank? && urls.length == 1
+              url = urls.first[:url]
+              extension = File.extname(url)
+              note.title = "File: #{extension[1..-1]}" unless extension.blank?
+            end
+
+            note.save!
+            note.url_entries.create!(urls)
+          end
+        end
+      end
+    end
+  end
+end
